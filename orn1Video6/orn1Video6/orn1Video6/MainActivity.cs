fe91ccc31c5181b9a54fb7436a9f236b32c3fcb7
recep tayyip fırat <rtf3444@gmail.com>
2018-04-21 21:17:37 +0300

    6.video 41.43 dakikadasın bu video ve 7.videoyu izlemicen simdik ileri xamarin videosundan devam ed

---
+++ orn1Video6/orn1Video6/orn1Video6/MainActivity.cs
@@ -0,0 +1,37 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace orn1Video6
+{
+    [Activity(Label = "orn1Video6", MainLauncher = true)]
+    public class MainActivity : Activity
+    {
+
+        Button btnLogin;
+        EditText TxtUserName;
+        EditText TxtPassword;
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Set our view from the "main" layout resource
+            /*hangı design eklersek o bunu alsın buda onun backendı dıyorum*/
+            SetContentView(Resource.Layout.Main);
+            btnLogin = FindViewById<Button>(Resource.Id.btnLogin);
+            TxtUserName = FindViewById<EditText>(Resource.Id.txtUserName);
+            TxtPassword = FindViewById<EditText>(Resource.Id.TxtPassworName);
+
+
+            btnLogin.Click += BtnLogin_Click;
+        }
+
+        private void BtnLogin_Click(object sender, System.EventArgs e)
+        {
+           Toast.MakeText(this,TxtUserName.Text,ToastLength.Long).Show()
+
+        }
+    }
+}
+
--- orn1Video6/orn1Video6/orn1Video6/Resources/Resource.Designer.cs
+++ orn1Video6/orn1Video6/orn1Video6/Resources/Resource.Designer.cs
@@ -44,8 +44,14 @@ namespace orn1Video6
 		public partial class Id
 		{
 			
+			// aapt resource value: 0x7f040001
+			public const int TxtPassworName = 2130968577;
+			
+			// aapt resource value: 0x7f040002
+			public const int btnLogin = 2130968578;
+			
 			// aapt resource value: 0x7f040000
-			public const int button1 = 2130968576;
+			public const int txtUserName = 2130968576;
 			
 			static Id()
 			{
