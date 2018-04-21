a45bfeb2f78ef486dfee8311ce6f224dc65fb2c6
recep tayyip fırat <rtf3444@gmail.com>
2018-04-21 18:23:44 +0300

    androidtradanionatal giris

---
+++ orn1Video6/orn1Video6/MainActivity.cs
@@ -0,0 +1,19 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace orn1Video6
+{
+    [Activity(Label = "orn1Video6", MainLauncher = true)]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+        }
+    }
+}
+
